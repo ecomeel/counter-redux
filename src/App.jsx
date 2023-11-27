f2cd2160a39bbb9227a2a0969183d43784ca6671
ecomeel <ecomeel@gmail.com>
2023-11-27 13:50:31 +0300

    add selector to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux"
+
+const getCounterValue = (state) => state.value
+
 export default function App() {
+    const counter = useSelector(getCounterValue)
     return (
-        <h1>Hello</h1>
+        <h1>{counter}</h1>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const store = createStore(counterReducer);
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>
 );
