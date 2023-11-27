6095977f8d2f794e9f0f667e2372250b66fb779b
ecomeel <ecomeel@gmail.com>
2023-11-27 14:00:29 +0300

    add handlers on buttons + dispatch these

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 
-const getCounterValue = (state) => state.value
+const getCounterValue = (state) => state.value;
 
 export default function App() {
-    const counter = useSelector(getCounterValue)
+    const counter = useSelector(getCounterValue);
+    const dispatch = useDispatch();
+
+    function handleDecrementButton() {
+        dispatch({
+            type: 'counter/decrement'
+        })
+    }
+
+    function handleIncrementButton() {
+        dispatch({
+            type: 'counter/increment'
+        })
+    }
+
     return (
-        <h1>{counter}</h1>
-    )
+        <div>
+            <p>{counter}</p>
+            <button onClick={handleIncrementButton}>+1</button>
+            <button onClick={handleDecrementButton}>-1</button>
+        </div>
+    );
 }
